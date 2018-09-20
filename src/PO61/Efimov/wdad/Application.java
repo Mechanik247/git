1da56d6517fe6db228a39bba714b4e86c8779c2b
mechanik247 <efimov.vladislav2011@yandex.ru>
2018-09-20 09:59:33 +0400

    Added entry point

---
+++ src/PO61/Efimov/wdad/Application.java
@@ -0,0 +1,9 @@
+import java.lang.System;
+
+public class Application
+{
+    public static void main(String[] args)
+    {
+        System.out.println("I'm Vladislav Efimov, and I'm not a monkey");
+    }
+}
